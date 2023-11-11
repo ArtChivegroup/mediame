d3599297cdb9f5739be4769fdf51ff6b82ec051c
ArtChivegroup <dmzartchive@gmail.com>
2023-11-12 00:56:08 +0700

    Commit otomatis

---
+++ git_app.py
@@ -0,0 +1,39 @@
+import os
+import tkinter as tk
+from tkinter import filedialog
+from git import Repo
+import shutil
+
+class GitUploaderLoaderApp:
+   def __init__(self, root):
+       self.root = root
+       self.root.title("Git Uploader dan Loader")
+
+       self.upload_button = tk.Button(root, text="Upload Perubahan", command=self.upload_perubahan)
+       self.upload_button.pack(pady=10)
+
+       self.load_button = tk.Button(root, text="Load Repository", command=self.load_repository)
+       self.load_button.pack(pady=10)
+
+   def upload_perubahan(self):
+       repo_path = filedialog.askdirectory(title="Pilih Repository Lokal")
+       if repo_path and os.path.exists(os.path.join(repo_path, ".git")):
+           repo = Repo(repo_path)
+           repo.git.add(".")
+           repo.git.commit("-m", "Commit otomatis")
+           repo.git.push("origin", repo.active_branch)
+       else:
+           print("Direktori yang dipilih bukan merupakan repository Git yang valid.")
+
+   def load_repository(self):
+       repo_path = filedialog.askdirectory(title="Pilih Folder Repository Lokal")
+       if repo_path:
+           if os.path.exists(repo_path) and os.path.isdir(repo_path):
+               shutil.rmtree(repo_path)
+           repo_url = "https://github.com/ArtChivegroup/mediame.git"
+           Repo.clone_from(repo_url, repo_path)
+
+if __name__ == "__main__":
+   root = tk.Tk()
+   app = GitUploaderLoaderApp(root)
+   root.mainloop()
